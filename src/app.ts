081daee268bbf95d51f708bc9709c44547b0cb7b
GleanCoder <adityakiranacharya0@gmail.com>
2026-03-09 13:01:09 +0530

    setup globalErrorHandler for better error handeling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import createHttpError from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
+  const err = createHttpError(400, "Something Went Wrong!");
+  throw err;
   res.send("Welcome to E-lib");
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,11 +4,13 @@ conf();
 interface configTypes {
   port: number;
   dbUrl: string;
+  env: string;
 }
 
 const _config: configTypes = {
   port: Number(process.env.PORT) || 3000,
   dbUrl: process.env.MONGO_CONNECTION_STRING as string,
+  env: process.env.NODE_ENV as string,
 };
 
 export const config = Object.freeze(_config);
