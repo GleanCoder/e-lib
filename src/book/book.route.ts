54034367c7162f2a8a379735e328cef532b623ee
GleanCoder <adityakiranacharya0@gmail.com>
2026-04-04 20:33:41 +0530

    setup multer for mutipart form

--- src/book/book.route.ts
+++ src/book/book.route.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import { createBook } from "./book.controller.js";
+import multer from "multer";
+import path from "node:path";
 
 const bookRouter = express.Router();
 
-bookRouter.post("/", () => {});
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 }, //30mb
+});
+
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook,
+);
 
 export default bookRouter;
